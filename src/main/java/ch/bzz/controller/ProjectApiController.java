4f7096411bb3263a2a6e1698e6d2a484c16f3c61
KrisHD1337 <kriscerni@gmail.com>
2025-10-28 11:51:49 +0100

    Added login and registration

--- src/main/java/ch/bzz/controller/ProjectApiController.java
+++ src/main/java/ch/bzz/controller/ProjectApiController.java
@@ -3,18 +3,45 @@ package ch.bzz.controller;
 import ch.bzz.generated.api.ProjectApi;
 import ch.bzz.generated.model.LoginProject200Response;
 import ch.bzz.generated.model.LoginRequest;
+import ch.bzz.model.Project;
+import ch.bzz.repository.ProjectRepository;
+import ch.bzz.util.JwtUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProjectApiController implements ProjectApi {
+
+    private final ProjectRepository projectRepository;
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private final JwtUtil jwtUtil = new JwtUtil();
+
+    public ProjectApiController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
     @Override
     public ResponseEntity<Void> createProject(LoginRequest loginRequest) {
-        return null;
+        String projectName = loginRequest.getProjectName();
+        if (!projectRepository.findByProjectName(projectName).isEmpty()) {
+            return ResponseEntity.status(409).build();
+        }
+        String hashedPassword = encoder.encode(loginRequest.getPassword());
+        loginRequest.setPassword(hashedPassword);
+        loginRequest.setProjectName(projectName);
+        projectRepository.save(new Project(projectName, hashedPassword));
+        return ResponseEntity.ok().build();
     }
 
     @Override
     public ResponseEntity<LoginProject200Response> loginProject(LoginRequest loginRequest) {
-        return null;
+        Project project = projectRepository.findByProjectName(loginRequest.getProjectName()).get(0);
+        if (project != null && encoder.matches(loginRequest.getPassword(), project.getPasswordHash())) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(401).build();
     }
 }
